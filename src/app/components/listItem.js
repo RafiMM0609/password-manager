0ed7cd55702c7ff759875683b5870e73ef6aac01
rafimahrus <mahrusrafi@gmail.com>
2025-08-10 18:06:51 +0700

    change button delete

--- src/app/components/listItem.js
+++ src/app/components/listItem.js
@@ -34,8 +34,10 @@ export function ListItem({ data, loading, error, deleteItem, onSelect }) {
     if (error) return <div className="flex items-center justify-center h-screen text-red-600">{error}</div>;
 
     return (
-        <div className="flex flex-col items-left justify-center h-screen bg-gray-400 text-gray-900 
-        space-y-2 p-2 rounded-4xl max-w-40">
+        <div className="flex flex-col items-left justify-center 
+        h-screen bg-gray-400 text-gray-900 
+        space-y-2 p-2 rounded-4xl max-w-60
+        overflow-y-auto">
             {decryptedData.length === 0 ? (
                 <div className="p-2 bg-white rounded-xl shadow">Tidak ada data</div>
             ) : (
@@ -44,7 +46,11 @@ export function ListItem({ data, loading, error, deleteItem, onSelect }) {
                         <p onClick={() => onSelect(item.id)}
                             className="truncate"
                             title={item.data_key}>{item.data_key}</p>
-                        <p onClick={(e) => deleteItem(e, item.id)} className="text-red-500 cursor-pointer">Delete</p>
+                        <button onClick={(e) => deleteItem(e, item.id)} className="text-red-500 cursor-pointer" title="Delete">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
                     </div>
                 ))
             )}
